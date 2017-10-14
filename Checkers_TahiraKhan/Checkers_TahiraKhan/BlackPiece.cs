bba15afb89b712098fb0f74fda48d15ce9f6518c
tahiragul <40227807@live.napier.ac.uk>
2017-10-14 20:59:18 +0100

    Added gui version

---
+++ Checkers_TahiraKhan/Checkers_TahiraKhan/BlackPiece.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkers_TahiraKhan
+{
+    public class BlackPiece : BoardPieces
+    {
+        public BlackPiece(int x, int y, string image): base(x,y,image)
+        {
+        }
+    }
+}
---
+++ Checkers_TahiraKhan_Gui/Checkers_TahiraKhan/WhitePiece.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkers_TahiraKhan
+{
+    public class WhitePiece : BoardPieces
+    {
+        public WhitePiece(int x, int y, string image) : base(x,y,image)
+        {
+
+        }
+    }
+}
